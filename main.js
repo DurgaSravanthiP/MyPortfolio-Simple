11c3a2c28dc40bffe92b908772c25a95f759a12a
Durga Sravanthi <sravanthi9637@gmail.com>
2025-10-27 22:07:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// Initialize typed.js for the `.text` element
+const typed = new Typed('.text', {
+    strings: ["Frontend Developer", "Web Developer", "MERN Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
