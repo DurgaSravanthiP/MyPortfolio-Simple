2634ab740cb60c2eec15793062165155d6b27753
Durga Sravanthi <sravanthi9637@gmail.com>
2026-01-30 14:12:55 +0530

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-// Initialize typed.js for the `.text` element
-const typed = new Typed('.text', {
-    strings: ["Frontend Developer", "Web Developer", "MERN Stack Developer"],
-    typeSpeed: 100,
-    backSpeed: 50,
-    backDelay: 1000,
-    loop: true
-});
